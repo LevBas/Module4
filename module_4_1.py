85a5121d307ad5dc76f9e65471c9a19bbb9cfeb4
LevBas <basalaevlev@yandex.ru>
2024-06-24 21:47:50 +0300

    Update module_4_1.py

--- module_4_1.py
+++ module_4_1.py
@@ -2,7 +2,5 @@ from fake_math import divide as fm
 from true_math import divide as tm
 first = int(input('Введите делимое: '))
 second = int(input('Введите делитель: '))
-print('В школьной математике ответ будет:')
-fm(first, second)
-print('В высшей математике ответ будет:')
-tm(first, second)
+print('В школьной математике ответ будет:', fm(first, second))
+print('В высшей математике ответ будет:', tm(first, second))

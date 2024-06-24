88649fc9fadc5ac5a12ce0e6be70b915b2cb79b8
LevBas <basalaevlev@yandex.ru>
2024-06-24 21:48:29 +0300

    Update true_math.py

--- true_math.py
+++ true_math.py
@@ -2,6 +2,7 @@ from math import inf
 
 def divide(first, second):
     if second == 0:
-        print(inf)
+        return inf
     else:
-        print(first / second)
+        result = first / second
+        return result
